05073635f1bb5638f8e3512a7548e9ed6dcd5a21
sonny <britzsky@gmail.com>
2026-04-02 13:48:15 +0900

    설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
-/**  @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "blogthumb.pstatic.net",
-            },
-        ],
-    },
+  // API 라우트를 유지하면서 빌드 산출물 폴더를 out으로 지정
+  distDir: "out",
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blogthumb.pstatic.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
